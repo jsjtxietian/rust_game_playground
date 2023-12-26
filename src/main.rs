ad19152ae685768375cf54b567370f9101e8ddcb
jsjtxietian <jsjtxietian@outlook.com>
2023-12-26 21:46:03 +0800

    Support choosing an algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
+use bevy_game::Guesser;
+use clap::{ArgEnum, Parser};
+
 const GAMES: &str = include_str!("../answers.txt");
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, arg_enum)]
+    implementation: Implementation,
+
+    #[clap(short, long)]
+    max: Option<usize>,
+}
+
+#[derive(ArgEnum, Debug, Clone, Copy)]
+enum Implementation {
+    Naive,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    match args.implementation {
+        Implementation::Naive => {
+            play(bevy_game::algorithms::Naive::new, args.max);
+        }
+    }
+}
+
+fn play<G>(mut mk: impl FnMut() -> G, max: Option<usize>)
+where
+    G: Guesser,
+{
     let w = bevy_game::Wordle::new();
-    for answer in GAMES.split_whitespace() {
-        let guesser = bevy_game::algorithms::Naive::new();
+    for answer in GAMES.split_whitespace().take(max.unwrap_or(usize::MAX)) {
+        let guesser = (mk)();
         if let Some(score) = w.play(answer, guesser) {
             println!("guessed '{}' in {}", answer, score);
         } else {
