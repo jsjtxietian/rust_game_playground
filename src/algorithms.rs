a2e11a1c3d1a4b8d6da0f71a3c3dd85514bfdea9
jsjtxietian <jsjtxietian@outlook.com>
2024-01-01 01:59:03 +0800

    weight, prune pattern and likely

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -2,8 +2,11 @@ mod allocs;
 mod naive;
 mod once_init;
 mod vecremain;
+mod prune;
 
 pub use allocs::Allocs;
 pub use naive::Naive;
 pub use once_init::OnceInit;
 pub use vecremain::Vecrem;
+pub use prune::Prune;
+
--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ enum Implementation {
     Allocs,
     Vecrem,
     Once,
+    Prune,
 }
 
 fn main() {
@@ -37,6 +38,9 @@ fn main() {
         Implementation::Once => {
             play(bevy_game::algorithms::OnceInit::new, args.max);
         }
+        Implementation::Prune => {
+            play(bevy_game::algorithms::Prune::new, args.max);
+        }
     }
 }
 
@@ -45,12 +49,17 @@ where
     G: Guesser,
 {
     let w = bevy_game::Wordle::new();
+    let mut score = 0;
+    let mut games = 0;
     for answer in GAMES.split_whitespace().take(max.unwrap_or(usize::MAX)) {
         let guesser = (mk)();
-        if let Some(score) = w.play(answer, guesser) {
-            println!("guessed '{}' in {}", answer, score);
+        if let Some(s) = w.play(answer, guesser) {
+            games += 1;
+            score += s;
+            println!("guessed '{}' in {}", answer, s);
         } else {
             println!("Failed to guess");
         }
     }
+    println!("average score: {:.2}", score as f64 / games as f64);
 }
