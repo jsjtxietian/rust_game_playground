16a3f26b176a79fd9a1963bd4a0584f3ea448328
jsjtxietian <jsjtxietian@outlook.com>
2024-01-31 22:45:20 +0800

    sapceship and asteroid, asset_server

---
+++ src/asteroids.rs
@@ -0,0 +1,72 @@
+use bevy::prelude::*;
+use rand::Rng;
+use std::ops::Range;
+
+use crate::{
+    asset_loader::SceneAssets,
+    movement::{Acceleration, MovingObejectBundle, Velocity},
+};
+
+const VELOCITY_SCALER: f32 = 5.0;
+const ACCELERATION_SCALER: f32 = 1.0;
+const SPAWN_RANGE_X: Range<f32> = -25.0..25.0;
+const SPAWN_RANGE_Z: Range<f32> = 0.0..25.0;
+const SPAWN_TIMER_SECONDS: f32 = 1.0;
+
+#[derive(Component, Debug)]
+pub struct Asteroid;
+
+#[derive(Resource, Debug)]
+pub struct SpawnTimer {
+    timer: Timer,
+}
+
+pub struct AsteroidPlugin;
+
+impl Plugin for AsteroidPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(SpawnTimer {
+            timer: Timer::from_seconds(SPAWN_TIMER_SECONDS, TimerMode::Repeating),
+        })
+        .add_systems(Update, spawn_asteroid);
+    }
+}
+
+fn spawn_asteroid(
+    mut commands: Commands,
+    mut spawn_timer: ResMut<SpawnTimer>,
+    time: Res<Time>,
+    scene_assets: Res<SceneAssets>,
+) {
+    spawn_timer.timer.tick(time.delta());
+    if !spawn_timer.timer.just_finished() {
+        return;
+    }
+
+    let mut rng = rand::thread_rng();
+
+    let translation = Vec3::new(
+        rng.gen_range(SPAWN_RANGE_X),
+        0.0,
+        rng.gen_range(SPAWN_RANGE_Z),
+    );
+
+    let mut random_unit_vector =
+        || Vec3::new(rng.gen_range(-1.0..1.0), 0.0, rng.gen_range(-1.0..1.0)).normalize_or_zero();
+
+    let velocity = random_unit_vector() * VELOCITY_SCALER;
+    let acceleration = random_unit_vector() * ACCELERATION_SCALER;
+
+    commands.spawn((
+        MovingObejectBundle {
+            velocity: Velocity::new(velocity),
+            acceleration: Acceleration::new(acceleration),
+            model: SceneBundle {
+                scene: scene_assets.asteroid.clone(),
+                transform: Transform::from_translation(translation),
+                ..default()
+            },
+        },
+        Asteroid,
+    ));
+}
---
+++ src/spaceship.rs
@@ -0,0 +1,31 @@
+use bevy::prelude::*;
+
+use crate::{
+    asset_loader::SceneAssets,
+    movement::{Acceleration, MovingObejectBundle, Velocity},
+};
+
+const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
+const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+
+pub struct SpaceshipPlugin;
+
+impl Plugin for SpaceshipPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PostStartup, spawn_spaceship);
+    }
+}
+
+fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
+    commands.spawn(MovingObejectBundle {
+        velocity: Velocity {
+            value: STARTING_VELOCITY,
+        },
+        acceleration: Acceleration { value: Vec3::ZERO },
+        model: SceneBundle {
+            scene: scene_assets.spaceship.clone(),
+            transform: Transform::from_translation(STARTING_TRANSLATION),
+            ..default()
+        },
+    });
+}
